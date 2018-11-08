103026e622478539d9eaab783d97dfb30d6b652a
paulinohuerta <paulinohuerta@gmail.com>
2018-11-08 09:18:14 +0100

    some improvements

--- scripsinmodulo.pl
+++ scripsinmodulo.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-print "Content-Type: text/html\n\n";
+print "Content-Type: text/html; charset=utf-8\n\n";
 if($ENV{'QUERY_STRING'} eq "") {
 print "<B>Introduce tu nombre</B>
        <form name=\"form1\" method=\"get\" >";
@@ -9,47 +9,43 @@ print "<input type=\"submit\" value=\"enviar\">";
 else{
 	@arGet=split("=",$ENV{'QUERY_STRING'});
 	$arGet[1]=~ s/%2F/\//g;
-########################################################################
 	if($arGet[0] eq "nombre"){
 	print "<B>Te llamas</B>
         <form name=\"form2\" method=\"get\" >";
 	print $arGet[1];
 	print "<input type=\"submit\" name=\"si\" value=\"si\" >";
 	}
-########################################################################
-if($arGet[0] eq "si") {
-   print "<h1>¿Que usario crees mas apropiado para un usuario que no sabe informatica?</h1>
+        if($arGet[0] eq "si") {
+          print "<h1>¿Qué usario crees más apropiado para un usuario que no sabe informática?</h1>
           <form name=\"Enviar\" method=\"get\" >";
-   open F, "fichero.txt";
-   while(<F>) {
-     chomp;
-     @campos=split(":");
-     $ar{$campos[0]}=$campos[0];
-   }
-   print "<select name = \"usuario\">";
-   for (keys %ar){
-     print "<option value=$_";
-     print ">$_</option>";
-   }
-   print "</select>
-         <input type=\"submit\" value=\"Enviar\" >";
-}
-##############################################################################################
-if($arGet[0] eq "usuario") {
-open T,">>/tmp/resultencuesta";
-   @arGet=split("=",$ENV{'QUERY_STRING'});
-   $arGet[1]=~ s/%2F/\//g;
-   print "<br>";
-   if($arGet[1] eq "standar"){
-    print "<h2>Elegiste standar,Gracias por su aportacion</h2>";
-    print T "standar";
-  }
-   if ($arGet[2] eq "root"){
-    print "<h2>Elegiste root,gracias por su aportacion</h2>";
-    print T "root"
-}
-}
-
-#############################################################################################333
-
+          open F, "fichero.txt";
+          while(<F>) {
+            chomp;
+            push @ar, $_
+          }
+          print "<select name = \"usuario\">";
+          for (@ar){
+            print "<option value=$_";
+            print ">$_</option>";
+          }
+          print "</select><input type=\"submit\" value=\"Enviar\" >";
+        }
+        if($arGet[0] eq "usuario") {
+          open T,">>/tmp/resultencuesta";
+          @arGet=split("=",$ENV{'QUERY_STRING'});
+          #$arGet[1]=~ s/%2F/\//g;
+          print "<br>";
+          open F, "fichero.txt";
+          while(<F>) {
+            chomp;
+            $values{$_} = 1
+          }
+          if(exists($values{$arGet[1]})){
+             print "<h2>Elegiste $arGet[1], gracias por tu aportación</h2>";
+             print T "$arGet[1]\n"
+          }
+          else {
+             print "<h2>No es correcta la elección</h2>";
+          }
+        }
 }
